9c46d28eb6765b7573c7be892f314adae20c5654
jburnoo <6pizz@naver.com>
2024-03-22 14:30:32 +0900

    add: room & ksp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -74,5 +75,9 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:$nav_version")
     // viewModel
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
-
+    // room
+    val room_version = "2.6.1"
+    implementation("androidx.room:room-runtime:$room_version")
+    ksp("androidx.room:room-compiler:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
 }
