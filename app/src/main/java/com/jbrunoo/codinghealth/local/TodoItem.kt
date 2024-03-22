8e85e30929697f545f5c62f25e25dcbdc19b40ff
jburnoo <6pizz@naver.com>
2024-03-22 15:00:03 +0900

    move package

---
+++ app/src/main/java/com/jbrunoo/codinghealth/local/TodoItem.kt
@@ -0,0 +1,15 @@
+package com.jbrunoo.codinghealth.local
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.UUID
+
+@Entity(tableName = "todo")
+data class TodoItem(
+    @PrimaryKey(autoGenerate = true) private val id: Long = 0,
+    val body: String,
+    val checked: Boolean = false
+)
