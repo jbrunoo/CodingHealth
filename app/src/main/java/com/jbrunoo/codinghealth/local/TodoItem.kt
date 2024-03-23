42710fd8fbb929d0088ba7be15194895c10234b9
jburnoo <6pizz@naver.com>
2024-03-24 01:31:51 +0900

    style: variable checked -> complete add: variable delete

--- app/src/main/java/com/jbrunoo/codinghealth/local/TodoItem.kt
+++ app/src/main/java/com/jbrunoo/codinghealth/local/TodoItem.kt
@@ -1,16 +1,13 @@
 package com.jbrunoo.codinghealth.local
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import java.util.UUID
 
 @Entity(tableName = "todo")
 data class TodoItem(
     @PrimaryKey(autoGenerate = true) val id: Long = 0,
     val body: String = "",
-    val checked: Boolean = false
+    val complete: Boolean = false,
+    @ColumnInfo(name = "delete", defaultValue = "0") val delete: Boolean = false
 )
