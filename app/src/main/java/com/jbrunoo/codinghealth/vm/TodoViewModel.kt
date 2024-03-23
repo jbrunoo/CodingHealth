0466ed12e6c317cc28e2816a2829fc21fb48f502
jburnoo <6pizz@naver.com>
2024-03-24 01:33:08 +0900

    style: rename func

--- app/src/main/java/com/jbrunoo/codinghealth/vm/TodoViewModel.kt
+++ app/src/main/java/com/jbrunoo/codinghealth/vm/TodoViewModel.kt
@@ -4,20 +4,21 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.jbrunoo.codinghealth.local.TodoDao
 import com.jbrunoo.codinghealth.local.TodoItem
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 
 // db는 context가 필요하므로 viewmodel에서 초기화 하지 않고 dao를 받아서 처리.
-class TodoViewModel(private val todoDao: TodoDao): ViewModel() {
-    private var _todoItemList = MutableStateFlow<List<TodoItem>>(emptyList())
-    val todoItemList = _todoItemList.asStateFlow()
+class TodoViewModel(private val todoDao: TodoDao) : ViewModel() {
+    private var _todoItems = MutableStateFlow<List<TodoItem>>(emptyList())
+    val todoItems = _todoItems.asStateFlow()
 
     init {
         viewModelScope.launch {
-            todoDao.getAll().collect { todoList ->
-                _todoItemList.value = todoList
+            todoDao.getAll().collect { todos ->
+                _todoItems.value = todos
             }
         }
     }
@@ -27,19 +28,16 @@ class TodoViewModel(private val todoDao: TodoDao): ViewModel() {
             todoDao.insert(todo)
         }
     }
+
     fun updateTodo(todo: TodoItem) {
         viewModelScope.launch {
             todoDao.update(todo)
         }
     }
-    fun deleteTodo(todo: TodoItem) {
+
+    fun deleteCheckedItems() {
         viewModelScope.launch {
-            todoDao.delete(todo)
-        }
-    }
-    fun deleteTodos() {
-        viewModelScope.launch {
-            todoDao.deleteAll()
+            todoDao.deleteTodos()
         }
     }
 }
