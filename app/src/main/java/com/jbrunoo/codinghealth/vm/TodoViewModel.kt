cd46f9a9acccb00ff1292f6905e4986811964588
jburnoo <6pizz@naver.com>
2024-03-23 18:36:48 +0900

    feat: asynchronous & init style: rename func

--- app/src/main/java/com/jbrunoo/codinghealth/vm/TodoViewModel.kt
+++ app/src/main/java/com/jbrunoo/codinghealth/vm/TodoViewModel.kt
@@ -1,31 +1,45 @@
 package com.jbrunoo.codinghealth.vm
 
-import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
-import com.jbrunoo.codinghealth.model.TodoItem
+import androidx.lifecycle.viewModelScope
+import com.jbrunoo.codinghealth.local.TodoDao
+import com.jbrunoo.codinghealth.local.TodoItem
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 
-class TodoViewModel: ViewModel() {
-    var todoItemList = mutableStateListOf<TodoItem>()
-        private set
+// db는 context가 필요하므로 viewmodel에서 초기화 하지 않고 dao를 받아서 처리.
+class TodoViewModel(private val todoDao: TodoDao): ViewModel() {
+    private var _todoItemList = MutableStateFlow<List<TodoItem>>(emptyList())
+    val todoItemList = _todoItemList.asStateFlow()
 
-    fun addTodoItem(item: TodoItem) {
-        todoItemList.add(item)
-    }
-
-    fun removeTodoItem(item: TodoItem) {
-        todoItemList.remove(item)
-    }
-
-    fun undateTodoItem(item: TodoItem, newBody: String) {
-        todoItemList.find { it.id == item.id }?.let {
-            it.body = newBody
+    init {
+        viewModelScope.launch {
+            todoDao.getAll().collect { todoList ->
+                _todoItemList.value = todoList
+            }
         }
     }
 
-    fun checkItem(item: TodoItem, checked: Boolean) =
-        todoItemList.find { it.id == item.id }?.let {
-//            it.checked.value = checked
-            it.checked = checked
+    fun addTodo(todo: TodoItem) {
+        viewModelScope.launch {
+            todoDao.insert(todo)
+        }
+    }
+    fun updateTodo(todo: TodoItem) {
+        viewModelScope.launch {
+            todoDao.update(todo)
+        }
+    }
+    fun deleteTodo(todo: TodoItem) {
+        viewModelScope.launch {
+            todoDao.delete(todo)
+        }
+    }
+    fun deleteTodos() {
+        viewModelScope.launch {
+            todoDao.deleteAll()
+        }
     }
 }
