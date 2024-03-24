c5eb67979d4b944736f22934619029af8425d2d7
jburnoo <6pizz@naver.com>
2024-03-24 18:05:29 +0900

    feat: migration dagger-hilt

--- app/src/main/java/com/jbrunoo/codinghealth/vm/TodoViewModel.kt
+++ app/src/main/java/com/jbrunoo/codinghealth/vm/TodoViewModel.kt
@@ -4,13 +4,16 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.jbrunoo.codinghealth.data.local.TodoDao
 import com.jbrunoo.codinghealth.data.local.TodoItem
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 
-// db는 context가 필요하므로 viewmodel에서 초기화 하지 않고 dao를 받아서 처리.
-class TodoViewModel(private val todoDao: TodoDao) : ViewModel() {
+// db는 context 필요, dao를 받아서 처리. 차후 repo, useCase
+@HiltViewModel
+class TodoViewModel @Inject constructor(private val todoDao: TodoDao) : ViewModel() {
     private var _todoItems = MutableStateFlow<List<TodoItem>>(emptyList())
     val todoItems = _todoItems.asStateFlow()
 
